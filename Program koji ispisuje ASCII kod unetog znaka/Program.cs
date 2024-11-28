64801c600a469d40b71960e4a8dd03ede141da48
amelhasic23 <117470943+amelhasic23@users.noreply.github.com>
2024-11-28 08:45:00 +0100

    Commit

---
+++ Program koji ispisuje ASCII kod unetog znaka/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Unesite jedan karakter: ");
+        char karakter = Console.ReadKey().KeyChar; // Čita karakter sa tastature
+        Console.WriteLine($"\nASCII kod unetog karaktera '{karakter}' je: {(int)karakter}");
+    }
+}
